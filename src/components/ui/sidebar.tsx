1ea29209669de578a80c13a4379096093cca5eb8
LUCAS NOGUEIRA <luke_adm@hotmail.com>
2024-11-30 04:05:37 -0300

    Ajuste menu

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -73,7 +73,7 @@ const SidebarProvider = React.forwardRef<
     // We use openProp and setOpenProp for control from outside the component.
     const [_open, _setOpen] = React.useState(defaultOpen);
     const open = openProp ?? _open;
-    console.log("open", open);
+
     const setOpen = React.useCallback(
       (value: boolean | ((value: boolean) => boolean)) => {
         const openState = typeof value === "function" ? value(open) : value;
--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -6,17 +6,12 @@ import { AppSidebar } from "../../components/app-sidebar";
 export function AppLayout() {
   return (
     <div className="flex h-screen flex-col antialiased">
-      {/* Cabeçalho fixo no topo */}
       <Header />
-
       <SidebarProvider>
         <div className="flex flex-1 overflow-hidden">
-          {/* Sidebar sempre visível */}
           <AppSidebar />
-
-          {/* Conteúdo principal rolável */}
           <main className="flex flex-1 flex-col gap-4 overflow-y-auto p-4">
-            <SidebarTrigger className="z-11" />
+            <SidebarTrigger />
             <Outlet />
           </main>
         </div>
