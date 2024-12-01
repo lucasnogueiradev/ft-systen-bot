27e29e51a49a1eaaeb5a16d857cdf8b36b2b6a23
LUCAS NOGUEIRA <luke_adm@hotmail.com>
2024-12-01 09:26:02 -0300

    Ajuste routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Dashboard } from "./pages/app/dashboard/dashboard";
-import { SignIn } from "./pages/auth/signIn";
+// import { SignIn } from "./pages/auth/signIn";
 import { AppLayout } from "./pages/_layouts/app";
-import { AuthLayout } from "./pages/_layouts/auth";
+// import { AuthLayout } from "./pages/_layouts/auth";
 import { ConsultarBot } from "./pages/app/bot/consultarBot";
 
 export const router = createBrowserRouter([
@@ -21,14 +21,14 @@ export const router = createBrowserRouter([
     ],
   },
 
-  {
-    path: "/",
-    element: <AuthLayout />,
-    children: [
-      {
-        path: "/sign-in",
-        element: <SignIn />,
-      },
-    ],
-  },
+  // {
+  //   path: "/",
+  //   element: <AuthLayout />,
+  //   children: [
+  //     {
+  //       path: "/sign-in",
+  //       element: <SignIn />,
+  //     },
+  //   ],
+  // },
 ]);
