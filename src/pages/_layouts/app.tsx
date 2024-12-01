2ca98a61b1dc5c03244ac4fcc45d14e8b6565e89
LUCAS NOGUEIRA <luke_adm@hotmail.com>
2024-12-01 09:48:07 -0300

    Ajuste menu sempre true

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -2,33 +2,28 @@ import { Outlet } from "react-router-dom";
 import { Header } from "../../components/header";
 import { SidebarProvider } from "../../components/ui/sidebar";
 import { AppSidebar } from "../../components/app-sidebar";
-// import { useEffect } from "react";
-// import { useState } from "react";
-// import Cookies from "js-cookie";
+import { useEffect } from "react";
+import { useState } from "react";
+import Cookies from "js-cookie";
 
 export function AppLayout() {
-  // const [defaultOpen, setDefaultOpen] = useState<boolean>(true);
+  const [defaultOpen, setDefaultOpen] = useState<boolean>(true);
 
-  // useEffect(() => {
-  //   // Acessando o valor do cookie
-  //   const sidebarState = Cookies.get("sidebar:state");
-  //   const defaultOpenValue = sidebarState === "true";
-  //   setDefaultOpen(defaultOpenValue);
-  // }, []);
+  useEffect(() => {
+    // Acessando o valor do cookie
+    const sidebarState = Cookies.get("sidebar:state");
+    const defaultOpenValue = sidebarState === "true";
+    setDefaultOpen(defaultOpenValue);
+  }, []);
+  console.log("true", defaultOpen);
 
   return (
     <div className="flex min-h-screen flex-col antialiased bg-muted">
       <Header />
       {/* Provedor de contexto para o Sidebar */}
-      <SidebarProvider defaultOpen={true}>
-        {/* Cabeçalho */}
-
-        {/* Layout Principal */}
-
-        {/* Sidebar */}
+      <SidebarProvider defaultOpen={defaultOpen}>
         <AppSidebar />
 
-        {/* Conteúdo Principal */}
         <main className="flex flex-1 flex-col max-h-screen gap-4 p-8 pt-6">
           <Outlet />
         </main>
