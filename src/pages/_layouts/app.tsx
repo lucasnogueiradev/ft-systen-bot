43cd0b910efda3863eddff265a43f81aba693331
LUCAS NOGUEIRA <luke_adm@hotmail.com>
2024-12-01 09:49:15 -0300

    Ajuste menu sempre true

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -5,9 +5,10 @@ import { AppSidebar } from "../../components/app-sidebar";
 import { useEffect } from "react";
 import { useState } from "react";
 import Cookies from "js-cookie";
+import React from "react";
 
 export function AppLayout() {
-  const [defaultOpen, setDefaultOpen] = useState<boolean>(true);
+  const [defaultOpen, setDefaultOpen] = useState<boolean>(false);
 
   useEffect(() => {
     // Acessando o valor do cookie
@@ -17,11 +18,12 @@ export function AppLayout() {
   }, []);
   console.log("true", defaultOpen);
 
+  const [open, setOpen] = React.useState(false);
   return (
     <div className="flex min-h-screen flex-col antialiased bg-muted">
       <Header />
       {/* Provedor de contexto para o Sidebar */}
-      <SidebarProvider defaultOpen={defaultOpen}>
+      <SidebarProvider open={open} onOpenChange={setOpen}>
         <AppSidebar />
 
         <main className="flex flex-1 flex-col max-h-screen gap-4 p-8 pt-6">
